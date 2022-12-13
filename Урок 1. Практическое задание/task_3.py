d4116dc3b47e596328134ed5cf53d6973ee96a43
slin4444 <94636783+slin4444@users.noreply.github.com>
2022-12-13 12:12:10 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,6 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
-n = int(input('Введите число - '))
-sum_n = (n + int(str(n) * 2) + int(str(n) * 3))
-print('Сумма равна - ', sum_n)
+n = int(input(f'Введите число - '))
+sum_n = n + int(str(n) * 2) + int(str(n) * 3)
+print(f"Сумма равна - {sum_n}")
